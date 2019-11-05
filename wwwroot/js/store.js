68d05dad4f5709771f0a2bb478c9bfe78164f844
Julius Ravago <jravago@ivincihealth.com>
2019-11-04 21:54:08 -0700

    Todomvc using asp.net core and vue

---
+++ wwwroot/js/store.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+var STORAGE_KEY = 'todos-vuejs';
+
+var todoStorage = {
+    fetch: function () {
+        return JSON.parse(localStorage.getItem(STORAGE_KEY) || '[]');
+    },
+    save: function (todos) {
+        localStorage.setItem(STORAGE_KEY, JSON.stringify(todos));
+    }
+};
